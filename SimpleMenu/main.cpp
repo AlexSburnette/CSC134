e83d0d6ab42cb27161c7beb560d1f5a4ee2f9f14
AlexSburnette <70519001+AlexSburnette@users.noreply.github.com>
2022-03-28 10:51:40 -0400

    Changed code and added comments

--- SimpleMenu/main.cpp
+++ SimpleMenu/main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 
 using namespace std;
-
+//print menu
+//get input
+//if valid, do a thing
+//if not valid, repeat
 int main()
 {
-    int choice;
+    int choice = 0;
     cout<<"Main Menu"<<endl;
     cout<<"1) Option 1"<< endl;
     cout<<"2) Exit"<< endl;
+    cout<< "Choose: ";
     cin>>choice;
-
-    if(1==choice){
-        cout<< "Option 1"<<endl;
+    //cout << "You picked "<< choice<<endl;
+     if(1==choice){
+        cout<< "You picked option 1."<<endl;
     }
-    else if(2=choice){
-        cout<<"Exiting now. Thank you!"<<endl;
+    else if (2==choice){
+        cout<<"Exiting now. Goodbye!"<<endl;
     }
     else{
-        cout<<"Wrong Choice"<<endl;
+        cout<<"Wrong Choice, please try again."<<endl;
     }
+
     return 0;
 }
+
